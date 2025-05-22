4c84a84b8e6ade314017ae3810f1afb1bc2bfc3f
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-22 15:02:10 +0000

    c

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+# JWT Auth endpoints
+    path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#include finance routes 
+    path('api/finance/', include('finance.urls'))
 ]
