df694c9b56fbd80bf09123e42630cd8ec1862fbd
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-23 11:01:35 +0000

    backend migrations success

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -3,6 +3,7 @@ from .models import Transaction
 from .serializers import TransactionSerializer
 
 class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()  # adding queryset for drf
     serializer_class = TransactionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
