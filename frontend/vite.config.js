0f469b1c5fee8f9a9ca530dcdfa3870f6907b6d4
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-23 12:12:33 +0000

    c

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow external access since in codespaces
+    port: 5173, // set port 
+  }
 })
