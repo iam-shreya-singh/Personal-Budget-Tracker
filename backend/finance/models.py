d3745d46734d4e0ab44955fc91e2cac7a94b3ec6
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-23 10:54:51 +0000

    serializers error

--- backend/finance/models.py
+++ backend/finance/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.CharField(max_length=255, blank=True)
+    t_type = models.CharField(max_length=2, choices=[("IN", "Income"), ("EX", "Expense")])
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.t_type} ₹{self.amount}"
