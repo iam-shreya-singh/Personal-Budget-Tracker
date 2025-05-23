5d96bfaf280e3ff15fa17af8ae356798d9b77eaf
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-23 17:57:31 +0000

    c

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Allow external access since in codespaces
-    port: 5173, // set port 
-  }
+    host: true, // Allow external access ( in Codespaces)
+    port: 5173, // Set port
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), 
+  },
+  },
 })
